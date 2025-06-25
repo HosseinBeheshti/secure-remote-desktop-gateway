8c4360830de9ca0227bcbe3b91067ab985102a3b
HosseinBeheshti <h.beheshti92@gmail.com>
2025-06-25 11:45:36 +0100

    Refactor VNC setup script to streamline user configuration and enhance service management

--- gateway_config.sh
+++ gateway_config.sh
@@ -1,30 +1,24 @@
 #!/bin/bash
 
-# Network Configuration
+# --- Gateway User (for managing the server and VPN) ---
+# This user has full sudo access and is intended for server administration.
+export GATEWAY_USER="gateway"
+export GATEWAY_PASSWORD="change_this_strong_password"
+export GATEWAY_VNC_DISPLAY="1" # Port will be 5901
+export GATEWAY_VNC_RESOLUTION="1280x800"
+
+# --- VNC User (for general remote desktop use) ---
+# This user is for standard remote desktop access.
+export VNC_USER="vncuser"
+export VNC_PASSWORD="change_this_password_too"
+export VNC_DISPLAY="2" # Port will be 5902
+export VNC_RESOLUTION="1920x1080"
+
+# --- VPN Configuration ---
 export SERVER_PUBLIC_IP="YOUR_SERVER_PUBLIC_IP"
 export VPN_SERVER_PUBLIC_IP="YOUR_VPN_SERVER_PUBLIC_IP"
 export VPN_PPP_GATEWAY_IP="192.168.150.1"
-export REMOTE_PC_IP="YOUR_REMOTE_PC_IP"  # IP of the remote PC
-
-# VNC Server Settings
-export VNC_USER="vncuser"
-export VNC_PORT="5901"
-export VNC_PASSWORD="change_this_password"
-export VNC_RESOLUTION="1920x1080"
-
-# Define VNC users and their configurations
-VNC_USER1="vncuser1"
-VNC_PASSWORD1="vnc123456"
-VNC_PORT1="5901"
-VNC_DISPLAY1="1"
-
-VNC_USER2="vncuser2"
-VNC_PASSWORD2="vnc789012"
-VNC_PORT2="5902"
-VNC_DISPLAY2="2"
-
-
-# VPN Authentication
+export REMOTE_PC_IP="YOUR_REMOTE_PC_IP"
 export IPSEC_PSK="YOUR_IPSEC_PSK"
 export VPN_USERNAME="vpn_username"
 export VPN_PASSWORD="vpn_password"
